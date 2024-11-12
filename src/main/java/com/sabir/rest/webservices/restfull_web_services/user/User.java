2e4b332715f32fe513339b5a280a06dc47e4b461
Sabir <sabir.obul@gmail.com>
2024-11-12 05:13:02 +0000

    add Fileter

--- src/main/java/com/sabir/rest/webservices/restfull_web_services/user/User.java
+++ src/main/java/com/sabir/rest/webservices/restfull_web_services/user/User.java
@@ -2,6 +2,8 @@ package com.sabir.rest.webservices.restfull_web_services.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,8 +11,11 @@ public class User {
         
     private Integer id;
     @Size(min=2,message="Name should be at least 2 character")
+
+    @JsonProperty("user name")
     private String name;
     
+    @JsonProperty("date of birth")
     @Past(message="Birth data should be past")
     private LocalDate dob;
 
